4bab17d357d59bc15bc9d5adb038e501e236edb6
ko <go.nobpadon25@gmail.com>
2021-03-03 09:17:59 +0700

    add orderDetail

---
+++ src/app/modules/POS/SmileShop/Order/components/OrderDetail.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { CardContent, Card } from "@material-ui/core";
+
+function OrderDetail() {
+	return (
+		<div>
+			<Card elevation={3} style={{ marginLeft: 5 }}>
+				<CardContent>
+					You Order
+					55
+					555
+					55
+					55
+					55
+					55
+					55
+					55
+					55
+					55
+					555
+					55
+					
+				</CardContent>
+			</Card>
+
+		</div>
+	)
+}
+
+export default OrderDetail
--- src/app/modules/POS/SmileShop/Order/pages/OrderList.js
+++ src/app/modules/POS/SmileShop/Order/pages/OrderList.js
@@ -1,13 +1,22 @@
 import React from 'react'
+import { Grid } from "@material-ui/core";
 import OrderProductGroup from '../../Order/components/OrderProductGroup'
 import OrderProduct from '../../Order/components/OrderProduct'
 import OrderDialog from '../../Order/components/OrderDialog'
+import OrderDetail from '../../Order/components/OrderDetail'
 function OrderList() {
 	return (
 		<div>
-			<OrderProductGroup></OrderProductGroup>
-			<OrderProduct></OrderProduct>
-			<OrderDialog></OrderDialog>
+			<Grid container>
+				<Grid item xs={12} lg={9}>
+					<OrderProductGroup></OrderProductGroup>
+					<OrderProduct></OrderProduct>
+				</Grid>
+				<Grid item xs={12} lg={3}>
+					<OrderDetail></OrderDetail>
+				</Grid>
+				<OrderDialog></OrderDialog>
+			</Grid>
 
 		</div>
 	)
