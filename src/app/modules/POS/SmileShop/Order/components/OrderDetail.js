b2b546e0af3c163f91bae4866c71293f32d7bd26
ko <go.nobpadon25@gmail.com>
2021-03-06 21:39:37 +0700

    bar bar

--- src/app/modules/POS/SmileShop/Order/components/OrderDetail.js
+++ src/app/modules/POS/SmileShop/Order/components/OrderDetail.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import { List, ListItemText, Typography, Grid, ListItem, Card, IconButton, ListItemSecondaryAction, TextField } from "@material-ui/core";
+import { List, ListItemText, Typography, Grid, ListItem, Card, CardContent, IconButton, ListItemSecondaryAction, Divider } from "@material-ui/core";
 import * as orderRedux from "../../Order/_redux/orderRedux";
 import DeleteIcon from '@material-ui/icons/Delete';
+import * as commonValidators from '../../../../Common/functions/CommonValidators';
 import { useSelector, useDispatch } from "react-redux";
 import { useFormik } from "formik";
 
@@ -12,66 +13,68 @@ function OrderDetail() {
 	const formik = useFormik({
 		enableReinitialize: true,
 		initialValues: {
-		  test: orderReducer.orderSubtotal.subtotal,
+			test: orderReducer.orderSubtotal.subtotal,
 		},
-	  });
+	});
 
 	return (
 
 		<Card style={{ marginLeft: 5 }}>
-			<Typography variant="h6" component="p" style={{ marginLeft: 5 }}>
-				Order Detail
-          </Typography>
-			<Grid container>
-				<Grid item xs={12} lg={10}>
-					<List elevation={5} >
-						{orderReducer.orderDetail.map((item) => (
-							<ListItem key={item.productId} alignItems="flex-start" >
-								<ListItemText primary={`${item.productName} จำนวน :  ${item.productQuantity} ราคา ${item.productPrice * item.productQuantity}`} />
-								<ListItemSecondaryAction>
-									<IconButton edge="end" aria-label="delete"
-										onClick={() => {
-											debugger
-											let orderList = [...orderReducer.orderDetail]
-											let obj = orderList.find((obj => obj.productId === item.productId));
+			<CardContent>
+				<Typography variant="h6" component="p">
+					Order Detail
+          		</Typography>
+				<Grid container>
+					<Grid item xs={12} lg={10}>
+						<List elevation={5} >
+							<Divider fullWidth component="hr" flexItem={false} />
+							{orderReducer.orderDetail.map((item) => (
+								<ListItem key={item.productId} alignItems="flex-start" >
+									<ListItemText primary={`${item.productName} จำนวน :  ${item.productQuantity} ราคา ${item.productPrice * item.productQuantity}`} />
+									<Divider fullWidth component="hr" />
+									<ListItemSecondaryAction>
+										<IconButton edge="end" aria-label="delete"
+											onClick={() => {
+												debugger
+												let orderList = [...orderReducer.orderDetail]
+												let obj = orderList.find((obj => obj.productId === item.productId));
 
-											if (obj) {
+												if (obj) {
 
-												//edit qty -1
-												obj.productQuantity -= 1;
+													//edit qty -1
+													obj.productQuantity -= 1;
 
-												//เช็ค qty = 0 ไหม
-												if (obj.productQuantity !== 0) {
+													//เช็ค qty = 0 ไหม
+													if (obj.productQuantity !== 0) {
 
-													//qty != 0 edit qty -1 save redux
-													dispatch(orderRedux.actions.updateOrderDetail(orderList));
+														//qty != 0 edit qty -1 save redux
+														dispatch(orderRedux.actions.updateOrderDetail(orderList));
 
+													} else {
+
+														//qty = 0 remove array product
+														orderList.splice(obj, 1);
+														dispatch(orderRedux.actions.deleteorderDetail(obj));
+													}
 												} else {
-
-													//qty = 0 remove array product
-													orderList.splice(obj, 1);
-													dispatch(orderRedux.actions.deleteorderDetail(obj));
+													alert("product not found")
 												}
-											} else {
-												alert("product not found")
-											}
-										}}
-									>
-										<DeleteIcon />
-									</IconButton>
-								</ListItemSecondaryAction>
-							</ListItem>
-						))}
-					</List>
+											}}
+										>
+											<DeleteIcon />
+										</IconButton>
+									</ListItemSecondaryAction>
+								</ListItem>
+							))}
+							<Divider />
+						</List>
+					</Grid>
+					<Grid item xs={12} lg={10}>
+						<Typography style={{textAlign:'right'}}>{commonValidators.currencyFormat(formik.initialValues.test)}
+						</Typography>
+					</Grid>
 				</Grid>
-				<TextField
-					name="test"
-					label="Test"
-					required
-					fullWidth
-					value={formik.initialValues.test}
-				/>
-			</Grid>
+			</CardContent>
 		</Card>
 	)
 }
