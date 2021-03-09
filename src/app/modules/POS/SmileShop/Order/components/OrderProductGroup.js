7b2d0f31049a71af1ea915afed63c7aa2006a133
ko <go.nobpadon25@gmail.com>
2021-03-09 09:14:39 +0700

    edit get OrderProductGroup get by api

--- src/app/modules/POS/SmileShop/Order/components/OrderProductGroup.js
+++ src/app/modules/POS/SmileShop/Order/components/OrderProductGroup.js
@@ -1,11 +1,33 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React from 'react';
 import { Button, Card, CardContent, Grid } from "@material-ui/core";
 import { useSelector, useDispatch } from "react-redux";
 import * as orderRedux from "../../Order/_redux/orderRedux";
+import * as CONST from '../../../../../../Constants';
+import * as swal from "../../../../Common/components/SweetAlert";
+import Axios from "axios";
 
 function OrderProductGroup() {
 	const dispatch = useDispatch();
 	const orderReducer = useSelector(({ order }) => order);
+	const [productGroup, setProductGroup] = React.useState([])
+	const PRODUCTGROUP_API_URL = `${CONST.API_URL}/SmileShop/ProductGroups`
+
+	React.useEffect(() => {
+		Axios.get(PRODUCTGROUP_API_URL)
+			.then((res) => {
+				//bind data
+				if (res.data.isSuccess) {
+					setProductGroup(res.data.data)
+				} else {
+					//internal error
+					swal.swalError("Error", res.data.message);
+				}
+			}).catch((err) => {
+				//physical error
+				swal.swalError("Error", err.message);
+			})
+	}, [])
 
 	const handleGet = (id) => {
 		let objPayload =
@@ -25,34 +47,15 @@ function OrderProductGroup() {
 					justify="center"
 					alignItems="center"
 					spacing={3}>
-					<Grid item xs={12} lg={3} >
-						<Button variant="outlined" color="primary" onClick={()=>{
-							handleGet(4);
-						}} >
-							Food
-      				</Button>
-					</Grid>
-					<Grid item xs={12} lg={3}>
-						<Button variant="outlined" color="primary" onClick={()=>{
-							handleGet(7);
-						}}>
-							Drinks
-      				</Button>
-					</Grid>
-					<Grid item xs={12} lg={3}>
-						<Button variant="outlined" color="primary" onClick={()=>{
-							handleGet(6);
-						}}>
-							Snacks
-      				</Button>
-					</Grid>
-					<Grid item xs={12} lg={3}>
-						<Button variant="outlined" color="primary" onClick={()=>{
-							handleGet(8);
-						}}>
-							Others
-      				</Button>
-					</Grid>
+						{productGroup.map((productGroup) => (
+							<Grid item xs={12} lg={3} key={`product_${productGroup.id}`}>
+							<Button variant="outlined" color="primary" onClick={()=>{
+								handleGet(productGroup.id);
+							}} >
+								{productGroup.name}
+						  </Button>
+						</Grid>
+						))}
 				</Grid>
 			</CardContent>
 		</Card>
