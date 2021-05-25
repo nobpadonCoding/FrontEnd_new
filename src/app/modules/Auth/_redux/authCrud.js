cffae43c1d92558781b7c64a851a281732ff64f7
go <go.nobpadon25@gmail.com>
2021-05-25 09:18:04 +0700

    fix bug get role

--- src/app/modules/Auth/_redux/authCrud.js
+++ src/app/modules/Auth/_redux/authCrud.js
@@ -39,5 +39,18 @@ export function renewToken() {
 
 export function getRoles(token) {
   let decoded = jwt_decode(token);
-  return decoded.role;
+  let result = [];
+
+  if (!decoded.role) {
+    return []
+  }
+
+  //push role ลง array(fix bug role --> "r","o","l","e")
+  if (Array.isArray(decoded.role)) {
+    result = decoded.role
+  } else {
+    result.push(decoded.role)
+  }
+
+  return result;
 }
